7c4c66a38a025545b1ff6fa6cf7efd21179002a1
Romuru <pastelblue0721@gmail.com>
2022-04-19 23:35:19 +0900

    setting react-hook-form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,39 +1,53 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const [errorCheck, setErrorCheck] = useState("");
+// function ToDoList() {
+//   const [toDo, setToDo] = useState("");
+//   const [errorCheck, setErrorCheck] = useState("");
 
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
-    console.log(toDo);
-    setErrorCheck("");
-  };
+//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//     setToDo(value);
+//     console.log(toDo);
+//     setErrorCheck("");
+//   };
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-    if (toDo.length < 10) {
-      return setErrorCheck("제목은 10글자 이상 작성해주세요.");
-    }
-  };
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     console.log(toDo);
+//     if (toDo.length < 10) {
+//       return setErrorCheck("제목은 10글자 이상 작성해주세요.");
+//     }
+//   };
 
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          placeholder="할 일을 추가하세요."
-        />
-        <button>추가</button>
-        {errorCheck !== "" ? errorCheck : null}
-      </form>
-    </div>
-  );
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input
+//           onChange={onChange}
+//           value={toDo}
+//           placeholder="할 일을 추가하세요."
+//         />
+//         <button>추가</button>
+//         {errorCheck == "" ? null : errorCheck}
+//       </form>
+//     </div>
+//   );
+}
+
+function ToDoList(){
+    const {register, watch, handleSubmit} =useForm();
+    console.log(watch());
+    return(
+        <div>
+            <form>
+                <input {...register("title")}placeholder="제목을 작성하세요." />
+                <button>추가</button>
+            </form>
+        </div>
+    )
 }
 
 export default ToDoList;
