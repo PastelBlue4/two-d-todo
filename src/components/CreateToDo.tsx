ef55cc1d0ff9902a9379d00a155defa9dcb89623
Romuru <pastelblue0721@gmail.com>
2022-04-21 21:38:39 +0900

    todo list done  before refactoring

---
+++ src/components/CreateToDo.tsx
@@ -0,0 +1,5 @@
+function CreateTodo() {
+  return null;
+}
+
+export default CreateTodo;
---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { atom, useRecoilState } from "recoil";
+
+interface IForm {
+  toDo: string;
+}
+
+interface IToDo {
+  text: string;
+  id: number;
+  category: "TO_DO" | "DOING" | "DONE";
+}
+
+const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+});
+
+function ToDoList() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = ({ toDo }: IForm) => {
+    setToDos((oldToDos) => [
+      { text: toDo, id: Date.now(), category: "TO_DO" },
+      ...oldToDos,
+    ]);
+    setValue("toDo", "");
+  };
+  return (
+    <div>
+      <h1>할 일 리스트</h1>
+      <hr />
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input
+          {...register("toDo", {
+            required: "할 일을 작성하세요.",
+          })}
+          placeholder="할 일을 작성해보세요."
+        />
+        <button>추가</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id}>{toDo.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
