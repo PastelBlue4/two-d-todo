85ca24808a2b014b4c2c3efc6c1fd26515dde54e
Romuru <pastelblue0721@gmail.com>
2022-04-27 23:58:39 +0900

    ITodo

--- src/components/atom.tsx
+++ src/components/atom.tsx
@@ -1,4 +1,12 @@
+interface IToDo {
+  text: string;
+  id: number;
+  category: "TO_DO" | "DOING" | "DONE";
+
 export default function atom(){
+ 
+  }
+
     const toDoState = atom<IToDo[]>({
     key: "toDo",
     default: [],
